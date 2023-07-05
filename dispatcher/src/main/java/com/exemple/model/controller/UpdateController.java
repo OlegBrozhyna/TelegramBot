e8a0d902ac7cf56fc358cee476e867743505877b
OlegBrozhyna <olegbrogina@gmail.com>
2023-07-05 17:49:33 +0300

    Initial commit

---
+++ dispatcher/src/main/java/com/exemple/model/controller/UpdateController.java
@@ -0,0 +1,86 @@
+package com.exemple.model.controller;
+
+import com.exemple.model.service.UpdateProducer;
+import com.exemple.model.utils.MessageUtils;
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static com.exemple.model.RabbitQueue.*;
+
+@Component
+@Log4j
+public class UpdateController {
+    private TelegramBot telegramBot;
+    private MessageUtils messageUtils;
+    private UpdateProducer updateProducer;
+
+    public UpdateController(MessageUtils messageUtils, UpdateProducer updateProducer) {
+        this.messageUtils = messageUtils;
+        this.updateProducer = updateProducer;
+    }
+
+    public void registerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        if (update == null) {
+            log.error("Received update is null");
+            return;
+        }
+        if (update.getMessage() != null) {
+            distributeMessageByType(update);
+        } else {
+            log.error("Received unsupported message type " + update);
+
+        }
+    }
+
+    private void distributeMessageByType(Update update) {
+        var message = update.getMessage();
+        if (message.getText() != null) {
+            processTextMassage(update);
+        } else if (message.getDocument() != null) {
+            processDocMassage(update);
+        } else if (message.getPhoto() != null) {
+            processPhotoMassage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+
+        }
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Unsupported message type!");
+        setView(sendMessage);
+    }
+
+    private void setFileIsReceivedView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "File received! Processing...");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+    private void processPhotoMassage(Update update) {
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+
+    private void processDocMassage(Update update) {
+        updateProducer.produce(DOC_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processTextMassage(Update update) {
+        updateProducer.produce(TEXT_MESSAGE_UPDATE, update);
+    }
+
+}
