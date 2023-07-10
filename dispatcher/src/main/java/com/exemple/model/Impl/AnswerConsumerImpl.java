dedb0562ec610538fb2ffffc6ea6f1c887296cbe
OlegBrozhyna <olegbrogina@gmail.com>
2023-07-10 12:37:38 +0300

    update

---
+++ dispatcher/src/main/java/com/exemple/model/Impl/AnswerConsumerImpl.java
@@ -0,0 +1,25 @@
+package com.exemple.model.Impl;
+
+import com.exemple.model.controller.UpdateController;
+import com.exemple.model.service.AnswerConsumer;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import static com.exemple.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class AnswerConsumerImpl implements AnswerConsumer {
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+
+    }
+}
---
+++ node/src/main/java/com/exemple/service/ProducerService.java
@@ -0,0 +1,8 @@
+package com.exemple.service;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+public interface ProducerService {
+    void producerAnswer(SendMessage sendMessage);
+}
+
