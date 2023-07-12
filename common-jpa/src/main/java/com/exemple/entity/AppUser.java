3f66e4bb06860d85f647c5c5450b7de63d8ecac5
OlegBrozhyna <olegbrogina@gmail.com>
2023-07-12 10:53:18 +0300

    update

---
+++ common-jpa/src/main/java/com/exemple/entity/AppUser.java
@@ -0,0 +1,35 @@
+package com.exemple.entity;
+
+
+import com.exemple.entity.enums.UserState;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long telegramUserId;
+    @CreationTimestamp
+    private LocalDateTime firstLoginDate;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private Boolean isActive;
+    @Enumerated(EnumType.STRING)
+    private UserState state;
+
+
+}
+
--- node/src/main/java/com/exemple/service/impl/MainServiceImpl.java
+++ node/src/main/java/com/exemple/service/impl/MainServiceImpl.java
@@ -1,26 +1,42 @@
 package com.exemple.service.impl;
 
+import com.exemple.dao.AppUserDAO;
 import com.exemple.dao.RawDataDAO;
+import com.exemple.entity.AppUser;
 import com.exemple.entity.RawData;
+import com.exemple.entity.enums.UserState;
 import com.exemple.service.MainService;
 import com.exemple.service.ProducerService;
+import lombok.Builder;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 
+import static com.exemple.entity.enums.UserState.BASIC_STATE;
+
+@Builder
 @Service
 public class MainServiceImpl implements MainService {
     private final RawDataDAO rawDataDAO;
     private final ProducerService producerService;
 
-    public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService) {
+    private final AppUserDAO appUserDAO;
+
+    public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService, AppUserDAO appUserDAO) {
         this.rawDataDAO = rawDataDAO;
         this.producerService = producerService;
+        this.appUserDAO = appUserDAO;
     }
 
     @Override
     public void processTextMessage(Update update) {
         saveRawData(update);
+// save telegramUser
+        var textMessage = update.getMessage();
+        var telegramUser=textMessage.getFrom();
+        var appUser = findOrSaveAppUser(telegramUser);
+
 
         var message = update.getMessage();
         var sendMessage = new SendMessage();
@@ -28,6 +44,24 @@ public class MainServiceImpl implements MainService {
         sendMessage.setText("Hello from NODE");
         producerService.producerAnswer(sendMessage);
     }
+    // save telegramUser
+    private AppUser findOrSaveAppUser(User telegramUser) {
+        AppUser persistentAppUser = appUserDAO.findAppUserByTelegramUserId(telegramUser.getId());
+        if (persistentAppUser == null) {
+            AppUser transientAppUser = AppUser.builder()
+                    .telegramUserId(telegramUser.getId())
+                    .username(telegramUser.getUserName())
+                    .firstName(telegramUser.getFirstName())
+                    .lastName(telegramUser.getLastName())
+                    //TODO change the default value after adding registration
+                    .isActive(true)
+                    .state(BASIC_STATE)
+                    .build();
+            return appUserDAO.save(transientAppUser);
+        }
+        return persistentAppUser;
+    }
+
 
     private void saveRawData(Update update) {
 
